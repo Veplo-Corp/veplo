43dcdaed7a24e1a6f346c34381d710b933be1466
Nicolo-Merlini <nicolo.merlini@gmail.com>
2022-10-30 19:48:40 +0100

    gender and macrocategory in URL

--- components/molecules/DintorniLogo_Below_Header.tsx
+++ components/molecules/DintorniLogo_Below_Header.tsx
@@ -10,7 +10,7 @@ const DintorniLogo_Below_Header: React.FC<Props>= ({city, category, gender}) => 
     return (
         <div className='flex justify-between mb-5'>
             <h1 className="font-black text-xl md:text-5xl italic text-black-900  ">Dintorni {city && <span className='text-base md:text-2xl'>per {city?.charAt(0).toUpperCase() + city?.slice(1)}</span>}</h1>
-            {gender && category && <p className='m-auto mr-0 mb-0'>{gender?.charAt(0).toUpperCase() + gender?.slice(1)} | {category?.charAt(0).toUpperCase() + category?.slice(1)}</p>}
+            {gender && category && <p className='m-auto mr-0 mb-0'>{gender} | {category}</p>}
         </div>
     )
 }
---
+++ components/utils/get_Gender_and_Macrocategory.ts
@@ -0,0 +1,30 @@
+import resetParametersFromUrl from "./resetParameters_from_url";
+import toUpperCaseFirstLetter from "./uppercase_First_Letter";
+
+const getGenderandMacrocategory = (gender_macrocategory: string) => {
+    var n = gender_macrocategory.indexOf('-');
+    let macrocategory: null | string = null;
+    let gender: null | string = null;
+    
+    gender = resetParametersFromUrl(gender_macrocategory.split('-')[0]);
+    macrocategory = gender_macrocategory.substring(gender_macrocategory.indexOf('-') + 1);
+
+    if(gender !== 'uomo' && gender !== 'donna'){
+        throw new Error('Gender i can be uomo or donna');
+    }
+
+    if(!macrocategory || macrocategory === 'uomo' || macrocategory==='donna'){
+        macrocategory = null;
+    }
+    
+    
+    const element = {
+        gender,
+        macrocategory
+    }
+
+    return element;
+
+}
+
+export default getGenderandMacrocategory;
