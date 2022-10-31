0a51213073d06a4ac4dc9adca4ae258881a7f261
Nicolo-Merlini <nicolo.merlini@gmail.com>
2022-10-31 20:10:07 +0100

    setup crop image

--- components/organisms/Drawer_Add_Image.tsx
+++ components/organisms/Drawer_Add_Image.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Button } from '@chakra-ui/react'
 import BlackButton from '../atoms/BlackButton'
 import {
@@ -10,20 +10,103 @@ import {
     DrawerContent,
     DrawerCloseButton,
 } from '@chakra-ui/react'
+import ReactCrop, { PixelCrop } from 'react-image-crop'
+import 'react-image-crop/dist/ReactCrop.css'
+import { canvasPreview } from '../molecules/Canva_previews'
+import { useDebounceEffect } from '../utils/useDebounceEffect'
 
 const Drawer_Add_Image = () => {
     const [isOpen, setisOpen] = useState(true);
     const hiddenFileInput = useRef(null);
+    const [completedCrop, setCompletedCrop] = useState<PixelCrop>()
+    const [aspect, setAspect] = useState<number | undefined>(762 / 1100);
+    const previewCanvasRef = useRef<HTMLCanvasElement>(null)
+    const [imgSrc, setImgSrc] = useState('')
+    const imgRef = useRef<HTMLImageElement>(null)
+    const [url, seturl] = useState(null)
 
-    const onImageChange = () => {
 
+    const [crop, setCrop] = useState<any>({
+        unit: '%', // Can be 'px' or '%'
+        x: 17.53,
+        y: 11.00,
+        width: 65.99,  //762 diviso 5
+        height: 76.209,//1100 diviso 5
+    })
+
+    const onHanldeConfirm = () => {
+        console.log(previewCanvasRef.current);
     }
+
+
+
+
+
+
+
+
 
     // Programatically click the hidden file input element
     // when the Button component is clicked
     const handleClick = (event) => {
         hiddenFileInput.current.click();
     };
+
+
+
+    function onSelectFile(e: React.ChangeEvent<HTMLInputElement>) {
+        if (e.target.files && e.target.files.length > 0) {
+            //setCrop(undefined) // Makes crop preview update between images.
+            const reader = new FileReader()
+            reader.addEventListener('load', () =>
+                setImgSrc(reader.result?.toString() || ''),
+            )
+            reader.readAsDataURL(e.target.files[0])
+        }
+    }
+
+
+    function onImageLoad(e: React.SyntheticEvent<HTMLImageElement>) {
+        if (aspect) {
+            const { width, height } = e.currentTarget
+            setCrop(centerAspectCrop(width, height, aspect))
+        }
+    }
+
+    useDebounceEffect(
+
+        async () => {
+            if (
+                completedCrop?.width &&
+                completedCrop?.height &&
+                imgRef.current &&
+                previewCanvasRef.current
+            ) {
+
+                // We use canvasPreview as it's much faster than imgPreview.
+                canvasPreview(
+                    imgRef.current,
+                    previewCanvasRef.current,
+                    completedCrop,
+
+                )
+                    .then(canvas => {
+                        canvas.toBlob(function (blob) {
+                            const url = URL.createObjectURL(blob);
+                            seturl(url)
+                            console.log(blob);
+                            console.log(url); // this line should be here
+                        }, 'image/webp');
+                    })
+
+            }
+        },
+        300,
+        [completedCrop],
+    )
+
+
+
 
     return (
 
@@ -57,11 +140,43 @@ const Drawer_Add_Image = () => {
                     </div>
                     <input
                         ref={hiddenFileInput}
-                        type="file" id="file" multiple accept="image/*" 
+                        type="file" id="file" multiple accept="image/*"
                         className='hidden'
-                        onChange={onImageChange} />
+                        onChange={(e) => {
+                            onSelectFile(e);
+                        }} />
                 </DrawerHeader>
                 <DrawerBody>
+                    <ReactCrop
+                        className='w-full md:w-1/2'
+                        //onImageLoaded={setImage}
+                        crop={crop}
+                        onChange={(_, percentCrop) => setCrop(percentCrop)}
+                        onComplete={(c) => setCompletedCrop(c)}
+                        aspect={762 / 1100}
+                    >
+                        <img src={imgSrc} ref={imgRef} />
+                    </ReactCrop>
+                    <button onClick={onHanldeConfirm}>test</button>
+                    <div>
+                        {!!completedCrop && (
+                            <canvas
+
+                                ref={previewCanvasRef}
+                                style={{
+                                    border: '1px solid black',
+                                    objectFit: 'contain',
+                                    width: completedCrop.width,
+                                    height: completedCrop.height,
+                                }}
+                            />
+                        )}
+                    </div>
+                    <div>
+                        {!!url && (
+                            <img src={url} alt="" />
+                        )}
+                    </div>
 
                 </DrawerBody>
                 <DrawerFooter padding={0}>
