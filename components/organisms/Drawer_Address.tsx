9567e0ea5678f3a5ee9e664327c16d383abbb968
Nicolo-Merlini <nicolo.merlini@gmail.com>
2022-12-03 14:10:49 +0100

    update location function using position of the device

--- components/organisms/Drawer_Address.tsx
+++ components/organisms/Drawer_Address.tsx
@@ -14,31 +14,54 @@ import setUserAddress from '../utils/setUserAddress';
 import { setAddress } from '../../src/store/reducers/address_user';
 import Address_text_handle from '../molecules/Address_text_handle';
 
-const Drawer_Address: React.FC<{ openDrawerMath: number}> = ({ openDrawerMath }) => {
+const Drawer_Address: React.FC<{ openDrawerMath: number }> = ({ openDrawerMath }) => {
     const dispatch = useDispatch();
     let filterTimeout: any;
     const [addresses, setAddresses] = useState([]);
     const [isOpen, setisOpen] = useState(false);
-    const [loading, setLoading] = useState<boolean>(false)
+    const [loading, setLoading] = useState<boolean>(false);
+    const [lng_lat, setLng_lat] = useState<string>('12.6450501048306,42.5626616098155');
+
     useEffect(() => {
         //console.log(openDrawerMath);
         if (openDrawerMath !== 1 && openDrawerMath !== undefined) {
+            const geolocationAPI = navigator.geolocation;
+            geolocationAPI.getCurrentPosition((position) => {
+                const { coords } = position;
+                console.log(coords);
+                
+                setLng_lat(coords.longitude + ',' + coords.latitude)
+            }, (error) => {
+                
+            })
             setisOpen(true)
         }
+
     }, [openDrawerMath])
 
 
 
     const onChangeAddress = async (address_searched: string) => {
+
         setLoading(true)
         clearTimeout(filterTimeout)
         filterTimeout = setTimeout(async () => {
             if (address_searched === undefined || address_searched.length < 3) {
                 return setLoading(false)
             }
+
+            //get IP
+            // const res = await axios.get('https://geolocation-db.com/json/')
+            // console.log(res.data);
+            // console.log(res.data.IPv4)
+            // const ip = res.data.IPv4
             // Send the data to the server in JSON format.
             // API endpoint where we send form data.
-            const endpoint = `/api/mapbox/autocomplete-address?search_text=${address_searched}&type=user`
+            //const endpoint = `/api/mapbox/autocomplete-address?search_text=${address_searched}&type=user&user_ip=${ip}`
+            console.log(lng_lat);
+            
+
+            const endpoint = `/api/mapbox/autocomplete-address?search_text=${address_searched}&type=user&lng_lat=${lng_lat}`
 
             // Send the form data to our forms API on Vercel and get a response.
             const response = await fetch(endpoint)
@@ -47,6 +70,8 @@ const Drawer_Address: React.FC<{ openDrawerMath: number}> = ({ openDrawerMath })
             // If server returns the name submitted, that means the form works.
             const result = await response.json()
             console.log(result.data);
+
+
 
             setLoading(false)
             return setAddresses(result.data)
