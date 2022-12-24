57f5b532da57a5d3727ca1c854c26f27874e4773
Nicolo-Merlini <nicolo.merlini@gmail.com>
2022-12-24 15:01:15 +0100

    deploy

--- components/organisms/Login_or_Registration.tsx
+++ components/organisms/Login_or_Registration.tsx
@@ -1,4 +1,4 @@
-import { Button, Input } from '@chakra-ui/react'
+import { Box, Button, Heading, Highlight, Input } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import BlackButton from '../atoms/BlackButton'
 import resetPassword from '../../components/utils/resetPassword'
@@ -63,26 +63,51 @@ const Login_or_Registration: React.FC<{ handleSubmitToPage: any, handleType: any
     }
 
     return (
-
-        <>
-            <div className='flex justify-between w-full px-2 py-6'>
-                <form className="md:p-3 space-y-4 w-full md:w-1/2" onSubmit={handleSubmitForm}>
-                    <h1 className="font-black text-xl md:text-3xl italic text-black-900  ">{title}</h1>
+            <form className="md:p-3 space-y-4 lg:w-1/2 lg:ml-24 xl:ml-28" onSubmit={handleSubmitForm}>
+                <h1 className="font-black text-xl md:text-3xl italic text-black-900  ">{title}</h1>
+                <div>
+                    <div className="mt-1 flex rounded-sm md:w-96">
+                        <span className="inline-flex items-center rounded-l-md border-r-0 border-2 border-gray-900  bg-black px-3 text-sm text-white">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                            </svg>
+                        </span>
+                        <Input
+                            onBlur={emailErrorHandler}
+                            value={email}
+                            autoComplete={'on'}
+                            onChange={emailHandler}
+                            type="text" name="email" id="email"
+                            roundedLeft={0}
+                            roundedBottomRight={'md'}
+                            p={['10px', '12px']}
+                            focusBorderColor='gray.900'
+                            variant='unstyled'
+                            _hover={{
+                                borderColor: 'gray.900'
+                            }}
+                            borderWidth={2}
+                            borderColor={'gray.900'}
+                            className="block flex-1 rounded-r-md border-2 p-2 py-3 border-gray-900 focus:outline-none sm:text-sm placeholder-black"
+                            placeholder="email" />
+                    </div>
+                    {isValidEmail === false && <p className='text-sm md:text-xs text-red-600'>email non corretta</p>}
+                </div>
+                {type !== 'reset_password' &&
                     <div>
                         <div className="mt-1 flex rounded-sm md:w-96">
                             <span className="inline-flex items-center rounded-l-md border-r-0 border-2 border-gray-900  bg-black px-3 text-sm text-white">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
                                 </svg>
                             </span>
                             <Input
-                                onBlur={emailErrorHandler}
-                                value={email}
+                                onBlur={passwordErrorHandler}
+                                value={password}
+                                onChange={passwordHandler}
                                 autoComplete={'on'}
-                                onChange={emailHandler}
-                                type="text" name="email" id="email"
-                                roundedLeft={0}
-                                roundedBottomRight={'md'}
+                                type={showPassword ? 'text' : 'password'} name="password" id="password"
+                                rounded={0}
                                 p={['10px', '12px']}
                                 focusBorderColor='gray.900'
                                 variant='unstyled'
@@ -92,78 +117,49 @@ const Login_or_Registration: React.FC<{ handleSubmitToPage: any, handleType: any
                                 borderWidth={2}
                                 borderColor={'gray.900'}
                                 className="block flex-1 rounded-r-md border-2 p-2 py-3 border-gray-900 focus:outline-none sm:text-sm placeholder-black"
-                                placeholder="email" />
+                                placeholder="password" />
+
+                            <span className="inline-flex items-center rounded-r-md border-l-0 border-2 border-black   px-3 text-sm text-black" onClick={() => setshowPassword(!showPassword)}>
+                                {!showPassword && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>}
+                                {showPassword && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                </svg>
+                                }
+                            </span>
                         </div>
-                        {isValidEmail === false && <p className='text-sm md:text-xs text-red-600'>email non corretta</p>}
-                    </div>
-                    {type !== 'reset_password' &&
-                        <div>
-                            <div className="mt-1 flex rounded-sm md:w-96">
-                                <span className="inline-flex items-center rounded-l-md border-r-0 border-2 border-gray-900  bg-black px-3 text-sm text-white">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                                    </svg>
-                                </span>
-                                <Input
-                                    onBlur={passwordErrorHandler}
-                                    value={password}
-                                    onChange={passwordHandler}
-                                    autoComplete={'on'}
-                                    type={showPassword ? 'text' : 'password'} name="password" id="password"
-                                    rounded={0}
-                                    p={['10px', '12px']}
-                                    focusBorderColor='gray.900'
-                                    variant='unstyled'
-                                    _hover={{
-                                        borderColor: 'gray.900'
-                                    }}
-                                    borderWidth={2}
-                                    borderColor={'gray.900'}
-                                    className="block flex-1 rounded-r-md border-2 p-2 py-3 border-gray-900 focus:outline-none sm:text-sm placeholder-black"
-                                    placeholder="password" />
-
-                                <span className="inline-flex items-center rounded-r-md border-l-0 border-2 border-black   px-3 text-sm text-black" onClick={() => setshowPassword(!showPassword)}>
-                                    {!showPassword && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                    </svg>}
-                                    {showPassword && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
-                                    </svg>
-                                    }
-                                </span>
-                            </div>
-                            {isValidPassword === false && <p className='text-sm md:text-xs text-red-600'>la password deve contentere almeno 8 caratteri</p>}
-                        </div>}
-                    {type !== 'reset_password' &&
-                        <div className='w-full flex md:w-96 justify-end pt-2 md:pt-1'>
-                            <BlackButton disabled={!isValidEmail || !isValidPassword} typeButton='submit' element={type == 'registration' ? 'registrati' : 'accedi'} borderRadius={10} size={'md'}></BlackButton>
-                        </div>}
-                    {type === 'reset_password' && <div className='w-full flex justify-end pt-2 md:pt-1 md:w-96' onClick={() => resetPassword(email)}>
-                        <Button disabled={isValidEmail == null || false} onClick={() => resetPassword(email)} colorScheme={'orange'} borderRadius={10} size={'md'}>resetta password</Button>
+                        {isValidPassword === false && <p className='text-sm md:text-xs text-red-600'>la password deve contentere almeno 8 caratteri</p>}
                     </div>}
-                    <div className='flex'>
-                        {(type === 'registration') && <LoginButton />}
-                        {(type === 'reset_password') && <LoginButton />}
+                {type !== 'reset_password' &&
+                    <div className='w-full flex md:w-96 justify-end pt-2 md:pt-1'>
+                        <BlackButton disabled={!isValidEmail || !isValidPassword} typeButton='submit' element={type == 'registration' ? 'registrati' : 'accedi'} borderRadius={10} size={'md'}></BlackButton>
+                    </div>}
+                {type === 'reset_password' && <div className='w-full flex justify-end pt-2 md:pt-1 md:w-96' onClick={() => resetPassword(email)}>
+                    <Button disabled={isValidEmail == null || false} onClick={() => resetPassword(email)} colorScheme={'orange'} borderRadius={10} size={'md'}>resetta password</Button>
+                </div>}
+                <div className='flex'>
+                    {(type === 'registration') && <LoginButton />}
+                    {(type === 'reset_password') && <LoginButton />}
 
-                        {type === 'login' &&
-                            <div className='grid'>
-                                <p className=' text-black	'>non hai ancora un account?
-                                    <Button className='underline ml-1' onClick={() => handleType('registration')} variant='link' colorScheme={'black'}>
-                                        Registrati
-                                    </Button>
-                                </p>
-                                <p className='mt-1 text-black	'>hai dimenticato la tua password?
-                                    <Button className='underline md:ml-1' onClick={() => handleType('reset_password')} variant='link' colorScheme={'black'}>
-                                        Resetta password
-                                    </Button>
-                                </p>
-                            </div>
-                        }
-                    </div>
-                </form>
-            </div>
-        </>
+                    {type === 'login' &&
+                        <div className='grid'>
+                            <p className=' text-black	'>non hai ancora un account?
+                                <Button className='underline ml-1' onClick={() => handleType('registration')} variant='link' colorScheme={'black'}>
+                                    Registrati
+                                </Button>
+                            </p>
+                            <p className='mt-1 text-black	'>hai dimenticato la tua password?
+                                <Button className='underline md:ml-1' onClick={() => handleType('reset_password')} variant='link' colorScheme={'black'}>
+                                    Resetta password
+                                </Button>
+                            </p>
+                        </div>
+                    }
+                </div>
+
+            </form>
     )
 }
 
