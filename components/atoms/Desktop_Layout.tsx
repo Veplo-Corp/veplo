56099ea931ff6be0846baf79e57f461999cf2ed5
Nicolo-Merlini <nicolo.merlini@gmail.com>
2023-01-01 23:53:40 +0100

    deploy

--- components/atoms/Desktop_Layout.tsx
+++ components/atoms/Desktop_Layout.tsx
@@ -5,7 +5,7 @@ const Desktop_Layout:React.FC<{children:any}> = ({children}) => {
 
   const user = useSelector((state:any) => state.user.user);  
   return (
-    <div className={`p-3 pt-16 md:p-10  ${!user ? 'md:pt-28': 'md:pt-20'}`}>
+    <div className={`p-3 pt-16 md:p-10  ${!user ? 'md:pt-28': 'md:pt-20'} min-h-[1000px]`}>
         {children}
     </div>
   )
