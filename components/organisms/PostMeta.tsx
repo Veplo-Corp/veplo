6bf5caddbaaa839a75d2587f15ef38d49dc97165
Nicolo-Merlini <nicolo.merlini@gmail.com>
2023-01-04 16:05:43 +0100

    deploy

---
+++ components/organisms/PostMeta.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import React, { FC } from 'react'
+
+const PostMeta: FC<{ title: string, subtitle: string, image: string, description: string }> = ({ title, subtitle, image, description }) => {
+  return (
+    <Head>
+        <title>{title}</title>
+        <meta name="description" content={subtitle} />
+
+        {/* open-graph meta */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={subtitle} />
+        <meta property="og:image" content={image} />
+
+        {/* twitter card meta */}
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={image} />
+    </Head>
+    
+  )
+}
+
+export default PostMeta
