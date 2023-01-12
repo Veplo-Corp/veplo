d0c7824b49daea70269c9d7540aeb2c6b5b045a4
Nicolo-Merlini <nicolo.merlini@gmail.com>
2023-01-12 15:39:17 +0100

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="it">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/x-icon" href="/static/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
