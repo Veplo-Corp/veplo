1eeafc2a97d615d2251d923e67087760a2537e22
Nicolo-Merlini <nicolo.merlini@gmail.com>
2023-04-30 14:28:07 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -244,14 +244,14 @@ function MyApp({ Component, pageProps }: any /* AppProps */) {
       //console.log("finished");
       setLoading(false);
     };
-    Router.events.on("routeChangeStart", start);
-    Router.events.on("routeChangeComplete", end);
-    Router.events.on("routeChangeError", end);
-    return () => {
-      Router.events.off("routeChangeStart", start);
-      Router.events.off("routeChangeComplete", end);
-      Router.events.off("routeChangeError", end);
-    };
+    // Router.events.on("routeChangeStart", start);
+    // Router.events.on("routeChangeComplete", end);
+    // Router.events.on("routeChangeError", end);
+    // return () => {
+    //   Router.events.off("routeChangeStart", start);
+    //   Router.events.off("routeChangeComplete", end);
+    //   Router.events.off("routeChangeError", end);
+    // };
   }, []);
 
   useEffect(() => {
