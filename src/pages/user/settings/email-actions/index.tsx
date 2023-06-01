e4032d0f9430539b7d7fde2a177019201212c2d4
Nicolo-Merlini <nicolo.merlini@gmail.com>
2023-06-01 16:55:14 +0200

    Create index.tsx

---
+++ src/pages/user/settings/email-actions/index.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const index = () => {
+    const router = useRouter();
+    console.log((router));
+
+    return (
+        <div>index</div>
+    )
+}
+
+export default index
