1c696a698815c8e48a011b7301ecbec695de6e12
Nicolo-Merlini <nicolo.merlini@gmail.com>
2023-07-12 17:04:34 +0200

    Update colors.ts

--- components/mook/productParameters/colors.ts
+++ components/mook/productParameters/colors.ts
@@ -4,24 +4,23 @@ export const COLORS_TYPES: { name: string, type: Color[] }[] = [
     {
         name: 'clothes_colors',
         type: [
-            { name: "Argento", cssColor: "#C0C0C0" },
-            { name: "Arancione", cssColor: "#FFA500" },
-            { name: "Azzurro", cssColor: "#87CEEB" },
-            { name: "Beige", cssColor: "#F5F5DC" },
             { name: "Bianco", cssColor: "#FFFFFF" },
-            { name: "Blu", cssColor: "#0000FF" },
-            { name: "Giallo", cssColor: "#FFFF00" },
-            { name: "Grigio", cssColor: "#A9A9A9" },
-            { name: "Lilla", cssColor: "#C8A2C8" },
-            { name: "Marrone", cssColor: "#A52A2A" },
-            { name: "Multicolore", cssColor: "linear-gradient(0deg, rgba(169,99,224,1) 0%, rgba(52,190,164,1) 46%, rgba(155,214,6,1) 100%);" },
             { name: "Nero", cssColor: "#000000" },
-            { name: "Oliva", cssColor: "DarkOliveGreen" },
-            { name: "Oro", cssColor: "#FFD700" },
-            { name: "Petrolio", cssColor: "#0abab5" },
-            { name: "Rosa", cssColor: "#FFC0CB" },
+            { name: "Beige", cssColor: "#F5F5DC" },
+            { name: "Grigio", cssColor: "#A9A9A9" },
             { name: "Rosso", cssColor: "#FF0000" },
-            { name: "Verde", cssColor: "#2E8B57" }
+            { name: "Arancione", cssColor: "#FFA500" },
+            { name: "Giallo", cssColor: "#FFFF00" },
+            { name: "Oro", cssColor: "#FFD700" },
+            { name: "Blu", cssColor: "#0000FF" },
+            { name: "Verde", cssColor: "#2E8B57" },
+            { name: "Rosa", cssColor: "#FFC0CB" },
+            { name: "Marrone", cssColor: "#A52A2A" },
+            { name: "Lilla", cssColor: "#C8A2C8" },
+            { name: "Multicolore", cssColor: "linear-gradient(0deg, rgba(169,99,224,1) 0%, rgba(52,190,164,1) 46%, rgba(155,214,6,1) 100%);" },
+            { name: "Argento", cssColor: "#C0C0C0" },
+            { name: "Petrolio", cssColor: "#0abab5" },
+            { name: "Azzurro", cssColor: "#87CEEB" },
         ]
 
     }
