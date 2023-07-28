5e8acc6dfbbd674f01397d6195ce729d1f628771
Nicolo-Merlini <nicolo.merlini@gmail.com>
2023-07-28 13:35:26 +0200

    deploy

--- components/utils/handleErrorFirebase.ts
+++ components/utils/handleErrorFirebase.ts
@@ -34,6 +34,5 @@ export const handleErrorFirebase = (error: string) => {
                 title: 'Si è verificato un errore!',
                 description: "per ora è impossibile procedere. riprova più tardi"
             }
-
     }
 }
