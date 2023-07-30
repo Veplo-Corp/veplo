928ffae8ec10187ec0723a7e06272585eb3766a5
Nicolo-Merlini <nicolo.merlini@gmail.com>
2023-07-30 14:42:40 +0200

    Update expirationTimeTokenControll.ts

--- components/utils/expirationTimeTokenControll.ts
+++ components/utils/expirationTimeTokenControll.ts
@@ -2,15 +2,16 @@ import { auth } from "../../src/config/firebase";
 import { setAuthTokenInSessionStorage } from "./setAuthTokenInSessionStorage";
 
 const expirationTimeTokenControll: (expirationTime: string) => Promise<boolean> = (expirationTime: string) => {
+
+
     return new Promise(async (resolve, reject) => {
+        const user = auth.currentUser;
+
         //check on the expiration date return false if the expiration time is passed
-        if (new Date(expirationTime) > new Date()) {
-            const user = auth.currentUser;
+        if (new Date(expirationTime) >= new Date()) {
             const idToken = await user?.getIdToken(true)
             if (!idToken) {
-                if (typeof window !== 'undefined') {
-                    window.location.reload()
-                }
+                window.location.reload()
                 return resolve(true)
             }
             setAuthTokenInSessionStorage(idToken)
