63f44a23f66f4d0e48ba9dad1da169f1f4c0ae03
Nicolo-Merlini <nicolo.merlini@gmail.com>
2023-08-09 13:52:07 +0200

    deploy

--- components/utils/findParsedFilter.ts
+++ components/utils/findParsedFilter.ts
@@ -1,4 +1,4 @@
-import { ProductsFilter } from "../../src/pages/cerca/[...slug]";
+import { ProductsFilter } from "../../src/pages/cerca/[prodotti]/[...slug]";
 import { FilterAccepted } from "../atoms/TagFilter";
 import { CATEGORIES, Univers } from "../mook/categories";
 import { Color } from "../mook/colors";
