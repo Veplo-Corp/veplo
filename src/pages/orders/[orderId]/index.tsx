bcfddae9bf653c20a279625a510d788eb5a9e228
Nicolo-Merlini <nicolo.merlini@gmail.com>
2023-08-13 15:24:21 +0200

    deploy

--- src/pages/orders/[orderId]/index.tsx
+++ src/pages/orders/[orderId]/index.tsx
@@ -31,6 +31,8 @@ import { GTMEventType } from '../../../lib/analytics/eventTypes';
 import { gtag } from '../../../lib/analytics/gtag';
 import expirationTimeTokenControll from '../../../../components/utils/expirationTimeTokenControll';
 import { openModal } from '../../../store/reducers/globalModal';
+import PostMeta from '../../../../components/organisms/PostMeta';
+import NoIndexSeo from '../../../../components/organisms/NoIndexSeo';
 
 
 
@@ -151,8 +153,19 @@ const index = () => {
 
     }
 
+    console.log(router.asPath);
+
     return (
         <Desktop_Layout>
+
+            <PostMeta
+                canonicalUrl={'https://www.veplo.it' + router.asPath}
+                title={"Ordine | Veplo"}
+                subtitle={"Veplo è lo spazio dove trovare i migliori brand di abbigliamento e accessori made in Italy. Con Veplo sostieni la moda responsabile."}
+                image={"android-chrome-512x512.png"}
+                description={"Veplo è lo spazio dove trovare i migliori brand di abbigliamento e accessori made in Italy. Con Veplo sostieni la moda responsabile."}
+            />
+            <NoIndexSeo />
             {order ? (
                 <div
                     className='w-full md:w-11/12 lg:w-10/12 xl:w-7/12 m-auto md:mt4'
